f81bec98dcfd7b5d066fa36f50264253eea34556
Alex Trinh <alexander9821@gmail.com>
2022-02-24 17:52:29 -0800

    second commit

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ window.onload = function makeGrid() {
 
     for (let i = 1; i <= 10; i++) {
         
-        let row = document.createElement("tr");
+        let row = document.createElement("div");
         row.id = "row-" + i;
 
         table.appendChild(row);
         let row_x = document.getElementById("row-" + i);
 
         for (let j = 1; j <= 12; j++) {
-            let cell = document.createElement("td");
+            let cell = document.createElement("div");
             cell.id = "cell-" + i + "-" + j;
 
             row_x.appendChild(cell);
         }
     }
 
-    console.log('The Script will load now.');
+    console.log('grid loaded');
 }
 
 
