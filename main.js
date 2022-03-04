fdc5c14d2f4bf5b202d501d3013ba1dd3a1b192b
Alex Trinh <alexander9821@gmail.com>
2022-03-03 17:00:40 -0800

    visable buffer zone working

--- main.js
+++ main.js
@@ -21,6 +21,25 @@ window.onload = function makeGrid() {
     }
 
     console.log('grid loaded');
-}
 
+    let tbl = document.getElementById("buffer-grid");
+
+    for (let i = 1; i <= 4; i++) {
+        
+        let buf_row = document.createElement("div");
+        buf_row.id = "buffer-grid-row-" + i;
+
+        tbl.appendChild(buf_row);
+        let row_y = document.getElementById("buffer-grid-row-" + i);
+
+        for (let j = 1; j <= 24; j++) {
+            let buf_cell = document.createElement("div");
+            buf_cell.id = "buffer-grid-cell-" + i + "-" + j;
+
+            row_y.appendChild(buf_cell);
+        }
+    }
+
+    console.log('buffer grid loaded');
+}
 
